2d70e579496a094fd35b435d62ad9fc9a7f7578b
Cư Hoàng Ngọc <20520145@gm.uit.edu.vn>
2022-12-10 00:08:17 +0700

    set default value for balance and is_admin columns in ResgisteredUserController

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -24,14 +24,18 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
+
+        
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'balance' => 0,
+            'is_admin' => false,
         ]);
 
         event(new Registered($user));
